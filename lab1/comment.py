4ee18dbcc86176171abfed482c061ebe2355619a
Miras <serikkalievmiras@gmail.com>
2024-01-18 17:32:04 +0300

    'comment'

--- lab1/comment.py
+++ lab1/comment.py
@@ -1,4 +1,10 @@
 # this is a comment
-print("I will pass the statistics for 90+ even if Dzhankieva")
+print("I will pass the statistics for 90+ even if Dzhankieva , INSHAALLAH")
 # print("Hello, World!")
 print("Cheers, Mate!")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
