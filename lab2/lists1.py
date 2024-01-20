6db0bc2138e9f86d674ad27bd72ea2a5cc5ceb41
Miras <serikkalievmiras@gmail.com>
2024-01-20 17:26:28 +0300

    list1

---
+++ lab2/lists1.py
@@ -0,0 +1,18 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry"))  # list constructor
+print(thislist)
