920029e6f3edfc1820d534053c151525f31bed13
Miras <serikkalievmiras@gmail.com>
2024-01-20 12:03:09 +0300

    var2

--- =.py
+++
@@ -1,5 +0,0 @@
-a = int(input(""))
-a3 = a % 10
-a2 = (a % 100) // 10
-a1 = a // 100
-print(str(a) - str(max(a1, a2, a3)) - str(min(a1, a2, a3)))
--- stepik.py
+++
@@ -1,5 +0,0 @@
-a, b, c = int(input()), int(input()), int(input())
-my_list = [a, b, c]
-my_list.sort(reverse=True)
-for i in my_list:
-    print(i)
