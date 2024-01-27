2bdc592dd8fea0b2523baceded5f8932ee811259
Miras <serikkalievmiras@gmail.com>
2024-01-27 20:01:53 +0300

    tuple3

---
+++ lab2/tuple3.py
@@ -0,0 +1,28 @@
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple)  # this will raise an error because the tuple no longer exists
