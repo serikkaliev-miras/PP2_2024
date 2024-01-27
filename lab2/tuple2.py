3b1c128d3a86b181da1824d0581337cd947a1d11
Miras <serikkalievmiras@gmail.com>
2024-01-27 19:49:41 +0300

    tuple2

---
+++ lab2/tuple2.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])  # the negative is also allowed
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
