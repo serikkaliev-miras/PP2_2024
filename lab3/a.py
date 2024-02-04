a6a32f9db36bb4437970fd0074f47be998bb96be
Miras <serikkalievmiras@gmail.com>
2024-02-04 20:48:06 +0300

    classes

--- lab3/a.py
+++ lab3/a.py
@@ -1,42 +1,16 @@
-import random
-
-name = input("Hello! What is your name? \n")
-
-
-def guess():
-    x = random.randint(1, 20)
-    print(f"Well, {name}, I am thinking of a number between 1 and 20.")
-    b = False
-    i = 0
-    while (b == False):
-        print("Take a guess.")
-        i += 1
-        if (x == i):
-            print(f"Good job, KBTU! You guessed my number in {i} guesses!")
-            b = True
-        elif (x > i):
-            print("Your guess is too low.")
-        else:
-            print("Your guess is too high.")
+class Shape:
+    def area(self):
+        self.areaa = 0
 
 
-guess()
+class Square(Shape):
+    def __init__(self):
+        self.a = int(input(""))
+
+    def area(self):
+        self.areaa = self.a * self.a
+        print(self.areaa)
 
 
-# import random
-# def guess(s):
-#     r=random.randint(1,20)
-#     print(f"\nWell, {s}, I am thinking of a number between 1 and 20.")
-#     b=False
-#     i=0
-#     while(b==False):
-#         i+=1
-#         n=int(input("Take a guess\n"))
-#         if(r==n):
-#             print(f"\nGood job, {s}! You guessed my number in {i} guesses!")
-#             b=True
-#         elif(r>n):
-#             print("\nYour guess is too low.")
-#         else:
-#             print("\nYour guess is too high.")
-# guess(input("Hello! What is your name? \n"))
+d = Square()
+d.area()
--- lab3/classes.py
+++ lab3/classes.py
@@ -13,3 +13,21 @@ result.getstring()
 result.upper()
 
 # 2
+
+
+class Shape:
+    def area(self):
+        self.areaa = 0
+
+
+class Square(Shape):
+    def __init__(self):
+        self.a = int(input(""))
+
+    def area(self):
+        self.areaa = self.a * self.a
+        print(self.areaa)
+
+
+d = Square()
+d.area()
