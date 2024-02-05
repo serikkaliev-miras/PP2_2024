93fac819deb2c451f74923cdf1da8ac7b318b25e
Miras <serikkalievmiras@gmail.com>
2024-02-05 21:31:08 +0300

    classes

--- lab3/classes.py
+++ lab3/classes.py
@@ -1,4 +1,7 @@
 # 1
+import math
+
+
 class Myclass:
 
     def getstring(self):
@@ -52,3 +55,32 @@ class Rectangle(Shape):
 
 r = Rectangle(int(input("Enter length: ")), int(input("Enter width: ")))
 r.area()
+
+# 4
+
+
+class point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def show(self):
+        print(f"Points at the beginning : {self.x} , {self.y}")
+
+    def move(self, nx, ny):
+        self.x = nx
+        self.y = ny
+        print(f"New ponts : {self.x} , {self.y}")
+
+    def dis(self):
+        x2, y2 = int(input("")), int(input(""))
+        distance = math.sqrt((x2 - self.x)**2 + (y2 - self.y)**2)
+        print(f"Distance between two points : {distance}")
+
+
+x1, x2 = int(input("")), int(input(""))
+result = point(x1, x2)
+result.show()
+new_x, new_y = int(input("Enter new x: ")), int(input("Enter new y: "))
+result.move(new_x, new_y)
+result.dis()
