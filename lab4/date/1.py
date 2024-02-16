f6b45326c4be8b53945b72318b09c1c301305652
Miras <serikkalievmiras@gmail.com>
2024-02-16 23:46:01 +0300

    lab4

--- lab4/date/1.py
+++ lab4/date/1.py
@@ -1,3 +1,6 @@
 import datetime
 x = datetime.datetime.now()
 print(int(x.strftime("%d")) - 5)
+
+# x = datetime.datetime.now()
+# print(f"{x.year}-{x.month}-{x.day-5} {x.hour}:{x.minute}:{x.second}")
---
+++ lab4/date/2.py
@@ -0,0 +1,28 @@
+import datetime
+x = datetime.datetime.now()
+# 1
+
+# yesterday = (x - datetime.timedelta(days=1)).strftime("%A")
+# tomorrow = (x + datetime.timedelta(days=1)).strftime("%A")
+# print(yesterday, x.strftime("%A"), tomorrow)
+
+# 2
+
+weekdays = {
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+    "Sunday": 7
+}
+yesterday = int(x.strftime("%w")) - 1
+tomorrow = int(x.strftime("%w")) + 1
+for day, number in weekdays.items():
+    if yesterday == number:
+        print(f"{day}")
+print(x.strftime("%A"))
+for day, number in weekdays.items():
+    if tomorrow == number:
+        print(f"{day}")
