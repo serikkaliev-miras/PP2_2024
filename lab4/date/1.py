a7f2ab804e9b7eede4205832444270bf0d5d86b2
Miras <serikkalievmiras@gmail.com>
2024-02-16 23:46:48 +0300

    lab4

--- lab4/date/1.py
+++ lab4/date/1.py
@@ -1,6 +1,10 @@
 import datetime
+# 1
+
 x = datetime.datetime.now()
 print(int(x.strftime("%d")) - 5)
 
-# x = datetime.datetime.now()
-# print(f"{x.year}-{x.month}-{x.day-5} {x.hour}:{x.minute}:{x.second}")
+# 2
+
+x = datetime.datetime.now()
+print(f"{x.year}-{x.month}-{x.day-5} {x.hour}:{x.minute}:{x.second}")
