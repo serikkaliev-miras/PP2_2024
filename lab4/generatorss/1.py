b236d8b782e5d581579df9f3f70207160e0562e0
Miras <serikkalievmiras@gmail.com>
2024-02-17 18:30:07 +0300

    lab4

---
+++ lab4/generatorss/1.py
@@ -0,0 +1,28 @@
+# 1
+
+def square(n):
+    for i in range(n):
+        print(i**2)
+
+
+n = int(input())
+square(n)
+
+# 2
+
+
+# class MyClass:
+#     def __init__(self, limit):
+#         self.limit = limit
+#         self.current = 1
+
+#     def generate(self):
+#         while self.current <= self.limit:
+#             print(self.current**2)
+#             self.current += 1
+
+
+# n = int(input(""))
+# gen = MyClass(n)
+# for i in gen.generate():
+#     print(i)
