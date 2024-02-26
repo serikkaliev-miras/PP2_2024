b7eac467ff0ed95f855d5369320a093665a1c2bb
Miras <serikkalievmiras@gmail.com>
2024-02-26 14:35:25 +0300

    lab5

--- lab5/regex/2.py
+++ lab5/regex/2.py
@@ -5,3 +5,23 @@ if result:
     print(result)
 else:
     print("no")
+
+# 2
+
+
+def test(pattern, testData, expectedResult):
+    if re.search(pattern, testData) == expectedResult:
+        print("test is passed!")
+    elif re.search(pattern, testData) != None:
+        print("test is passed!")
+    else:
+        print("test is not passed!")
+
+
+pattern = 'ab{2,3}'
+
+test(pattern, "123ab452", None)
+test(pattern, "123abb452", True)
+test(pattern, "abbb452", True)
+test(pattern, "123ab45", None)
+test(pattern, "123ab45as", None)
