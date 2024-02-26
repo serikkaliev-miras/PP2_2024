99b7d7458ac612ca73bb2956ebad283f9777f505
Miras <serikkalievmiras@gmail.com>
2024-02-26 14:34:30 +0300

    lab5

--- lab5/regex/1.py
+++ lab5/regex/1.py
@@ -21,3 +21,23 @@ if result:
     print("yep")
 else:
     print("no")
+
+# 3
+
+
+def test(pattern, testinput, testoutput):
+    if re.search(pattern, testinput) == testoutput:
+        print("test is not passed")
+    elif re.search(pattern, testinput) != None:
+        print("test is passed!")
+    else:
+        print("test is not passed")
+
+
+pattern = '^a(b*).'
+
+test(pattern, "123ab45", None)
+test(pattern, "123ab45as", None)
+test(pattern, "123ab452", None)
+test(pattern, "abb", True)
+test(pattern, "abbb452", True)
